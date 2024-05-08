069135626943daa79c1a722b3a4a308a0732b5ab
wayzap <wayzap248@gmail.com>
2024-05-08 20:01:51 +0300

    First version of app

---
+++ app/src/main/java/com/example/pexelsapp/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.pexelsapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavigationBar: BottomNavigationView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        bottomNavigationBar = findViewById(R.id.bottomNavigationView)
+        bottomNavigationBar.itemIconTintList  = null
+    }
+}
